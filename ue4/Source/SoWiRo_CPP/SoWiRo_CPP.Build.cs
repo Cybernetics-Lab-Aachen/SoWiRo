e58e030d74077a3e4dfa3895e7f085b728523458
Fabian Unterstell <fabian.unterstell@ima.rwth-aachen.de>
2017-02-07 23:11:46 +0100

    update ue4 to 4.14

--- ue4/Source/SoWiRo_CPP/SoWiRo_CPP.Build.cs
+++ ue4/Source/SoWiRo_CPP/SoWiRo_CPP.Build.cs
@@ -8,19 +8,14 @@ public class SoWiRo_CPP : ModuleRules
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemNull", "OnlineSubsystemUtils" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
 		// Uncomment if you are using online features
-		PrivateDependencyModuleNames.Add("OnlineSubsystem");
-		if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-		{
-		    if (UEBuildConfiguration.bCompileSteamOSS == true)
-				{
-					DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-				}
-		 }
+		 PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
